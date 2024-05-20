7fa3ef81a772328d572a76e7b3200bd2a80732c4
yurii19 <yurikolechkin@gmail.com>
2024-05-20 16:58:50 +0300

    fix: styles

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -18,7 +18,7 @@ export class PlayerComponent implements OnInit {
   ];
 
   audioContext: AudioContext | undefined;
-  sources:AudioBufferSourceNode[] = [];
+  sources: AudioBufferSourceNode[] = [];
   track: MediaElementAudioSourceNode | undefined;
   gainNode!: GainNode | undefined;
 
@@ -34,41 +34,82 @@ export class PlayerComponent implements OnInit {
     }
   }
 
-  launchAudio() {
-    this.files.forEach(url =>{
-      this.loadAudioFile(url)
-      .then((buffer) => {
-        if(buffer)
-        this.playAudio(buffer);
-      })
-      .catch((error) => {
-        console.error('Error loading audio:', error);
-      });
+  async mergeBuffers() {
+    const tracks: AudioBuffer[] = [];
 
-    })
-   
+    for await (const url of this.files) {
+      const buffer = await this.loadAudioFile(url);
+      if (buffer) {
+        tracks.push(buffer);
+      }
+    }
+
+    console.log(tracks);
+    const firstTrack = tracks[0];
+    const duration =
+      firstTrack && firstTrack.duration ? firstTrack.duration : 0;
+    const rate =
+      firstTrack && firstTrack.sampleRate ? firstTrack.sampleRate : 0;
+    const numberOfChannels = this.files.length;
+    const frameCount = rate * duration;
+
+    if (!this.audioContext) return;
+
+    const newBuffer: AudioBuffer = this.audioContext.createBuffer(
+      numberOfChannels,
+      frameCount,
+      rate
+    );
+
+    for (let channel = 0; channel < numberOfChannels; channel++) {
+      const nowBuffering = newBuffer.getChannelData(channel);
+      const srcBuffer = tracks[channel].getChannelData(0);
+      // console.log(channel, ' < < < ', srcBuffer);
+      for (let i = 0; i < frameCount; i++) {
+        nowBuffering[i] = srcBuffer[i];
+        // console.log(srcBuffer[i])
+      }
+    }
+    console.log(newBuffer);
+    return newBuffer;
+  }
+
+  async launchAudio() {
+    const buffer = await this.mergeBuffers();
+    console.log(buffer);
+    if (buffer) {
+      this.playAudio(buffer);
+    }
+
+    // this.files.forEach((url) => {
+    //   this.loadAudioFile(url)
+    //     .then((buffer) => {
+    //       if (buffer) this.playAudio(buffer);
+    //     })
+    //     .catch((error) => {
+    //       console.error('Error loading audio:', error);
+    //     });
+    // });
   }
 
   playAudio(buffer: AudioBuffer) {
     if (this.audioContext && buffer) {
       const source = this.audioContext.createBufferSource();
       source.buffer = buffer;
-      this.sources.push(source)
-      if(this.gainNode){
+      this.sources.push(source);
+      if (this.gainNode) {
         source.connect(this.gainNode).connect(this.audioContext.destination);
-        
       }
-      source.start(0);
+      source.start(5);
     }
   }
 
-  loadAudioFile(url: string) {
-    return fetch(url)
-      .then((response) => response.arrayBuffer())
-      .then((buffer) => {
-        if (this.audioContext === undefined) return;
-        return this.audioContext.decodeAudioData(buffer);
-      });
+  async loadAudioFile(url: string) {
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
+    // console.log(buffer);
+    if (this.audioContext === undefined) return;
+    return this.audioContext.decodeAudioData(buffer);
   }
 
   changeGain(ev: Event) {
@@ -79,13 +120,12 @@ export class PlayerComponent implements OnInit {
     }
   }
 
-
-
   play() {
     this.launchAudio();
+    this.mergeBuffers();
   }
 
   pause() {
-    this.sources.forEach(src => src.stop())
+    this.sources.forEach((src) => src.stop());
   }
 }
