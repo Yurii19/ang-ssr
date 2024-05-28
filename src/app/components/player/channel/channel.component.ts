3c3c278c25eb31e69e9af935d2caa4d54d25cd37
yurii19 <yurikolechkin@gmail.com>
2024-05-28 11:29:42 +0300

    feat: canvas added

--- src/app/components/player/channel/channel.component.ts
+++ src/app/components/player/channel/channel.component.ts
@@ -1,9 +1,12 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   EventEmitter,
   Input,
   Output,
+  ViewChild,
 } from '@angular/core';
 //import { EventEmitter } from 'stream';
 
@@ -15,7 +18,7 @@ import {
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss',
 })
-export class ChannelComponent {
+export class ChannelComponent implements AfterViewInit {
   @Input() soundLevel: number = 10;
 
   @Input() name: number = 0;
@@ -25,6 +28,26 @@ export class ChannelComponent {
     channelId: number;
     volume: number;
   }>();
+
+  @ViewChild('visualizer')
+  private myCanvas: ElementRef = {} as ElementRef;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const canvasContext = this.myCanvas.nativeElement.getContext('2d');
+    const canvas = this.myCanvas.nativeElement;
+
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasContext.lineWidth = 2;
+    canvasContext.strokeStyle = 'lime';
+    canvasContext.beginPath();
+    canvasContext.moveTo(0, canvas.height / 2);
+    canvasContext.lineTo(300, 150);
+    canvasContext.stroke();
+  }
 
   mute(event: number) {
     this.muteChannel.emit(event);
