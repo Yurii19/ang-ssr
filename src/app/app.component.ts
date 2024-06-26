268daa98ea1c57a59b126117fab5406db8b372d3
yurii19 <yurikolechkin@gmail.com>
2024-06-26 13:53:32 +0300

    chore: routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayerComponent } from './components/player/player.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlayerComponent],
+  imports: [RouterOutlet, PlayerComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayerComponent } from './pages/player/player.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path:  'home', component:  HomeComponent},
+    { path:  'player', component:  PlayerComponent},
+];
