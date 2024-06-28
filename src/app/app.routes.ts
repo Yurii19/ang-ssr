781998ac00f0e0fbc05bea5f23ec9f7a60aada21
yurii19 <yurikolechkin@gmail.com>
2024-06-28 13:41:25 +0300

    chore: lazy loading added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { PlayerComponent } from './pages/player/player.component';
-
+//import { PlayerComponent } from './pages/player/player.component';
 
 export const routes: Routes = [
-    { path:  'home', component:  HomeComponent},
-    { path:  'player', component:  PlayerComponent},
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'player',
+    loadComponent: () =>
+      import('./components/player/player.component').then(
+        (m) => m.PlayerComponent
+      ),
+  },
 ];
